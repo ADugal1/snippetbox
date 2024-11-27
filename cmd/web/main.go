97814e1ad1b1173d1a09a51cfe7af51b7a36788d
Andrew <dugal_andrew@bah.com>
2024-11-27 17:59:05 -0400

    updates for logging and using flag.Parse

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	flag.Parse()
+
+	// Use the slog.New() function to initialize a new structured logger, which
+	// writes to the standard out stream and uses the default settings.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
 	mux := http.NewServeMux()
+
+	// file server
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// file server route
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// other application routes
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4000")
+	logger.Info("starting server on %s, *addr")
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	err := http.ListenAndServe(*addr, mux)
+
+	logger.Error(err.Error())
+	os.Exit(1)
 }
